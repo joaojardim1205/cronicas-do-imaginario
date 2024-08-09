f73e98b012112862bfa70e551baa78fc3525f06a
Joao jardim <joaopedrojardim1205@gmail.com>
2024-08-09 09:11:48 -0300

    arquivos do codigo-fonte

--- index.js
+++ index.js
@@ -1,58 +1,108 @@
 let progresso = 0
 let inventario = []
-let personagem
-
-
-function showChoices(choices) {
-    choices.forEach(choice => {
-        const button = document.createElement('button');
-        button.textContent = choice.text;
-        button.onclick = choice.action;
-        gameElement.appendChild(button);
-    });
-}
+let acessoInv
 
 function startGame() {
-    showText("Seu nome é Tachlowini, atualmente possui 12 anos e mora em Eritreia, sua terra natal.");
-    showText("Como Eritreia é um pais com uma taxa de pobreza muito alta, você depois de muito pensar decide ir embora em busca de uma vida melhor.");
-    showText("Você possui um amigo, seu unico melhor amigo. Antes de sair, você precisa tomar uma decisão muito importante. Chamar o seu amigo e ir embora com ele, ou ir sozinho.");
-    showChoices([
-        { text: "ir atras de seu amigo", action: goFriend },
-        { text: "Ir embora sozinho", action: goAlone }
-    ]);
+    alert("Seu nome é Tachlowini, atualmente possui 12 anos e mora em Eritreia, sua terra natal.");
+    alert("Como Eritreia é um pais com uma taxa de pobreza muito alta, você depois de muito pensar decide ir embora em busca de uma vida melhor.");
+    alert("Você possui um amigo, seu unico melhor amigo. Antes de sair, você precisa tomar uma decisão muito importante. Chamar o seu amigo e ir embora com ele, ou ir sozinho.");
+    let irAmigo = prompt ("Ir com o amigo? (sim) (nao)");
+
+    if(irAmigo == "sim"){
+        alert("você ganhou 1x empatia");
+        alert("empatia ira ficar armazenada no inventario");
+        alert("empatia ira influencia-lo no futuro");
+        inventario.push("1x empatia");
+
+        acessoInv = prompt ("você deseja acessar o inventario? (sim) (nao)");
+        if(acessoInv == "sim"){
+            alert(inventario);
+        } else if(acessoInv == "nao"){
+            alert("você nao acessou o inventario");
+        }
+
+        goFriend()
+    } else if(irAmigo == "nao"){
+        alert("você ganhou 1x antipatia");
+        alert("antipatia ira ficar armazenada no inventario");
+        alert("antipatia ira influencia-lo no futuro");
+        inventario.push("1x antipatia");
+
+        acessoInv = prompt ("você deseja acessar o inventario? (sim) (nao)");
+        if(acessoInv == "sim"){
+            alert(inventario);
+        } else if(acessoInv == "nao"){
+            alert("você nao acessou o inventario");
+        }
+        
+        goAlone()
+    } else {
+        alert("resposta invalida");
+    }
+       
 }
 
 function goFriend() {
-    showText("Você decidiu que ir atras de seu amigo, então você pega itens basicos e vai ate a casa dele.");
-    showText("Ao chegar na casa dele, você rapidamente o chama e o convida para ir com você. Ele prontamente responde que aceita e logo em seguida vão embora de seu pais natal.");
-    showText("Agora, os dois tem que decidir entre ir para a etiopia ou para o sultão, dois paises vizinhos de Eritreia.");
-    showChoices([
-        { text: "Ir à etiopia", action: goEtiopia },
-        { text: "Ir à sultão", action: goSultão }
-    ]);
+    alert("Você decidiu que ir atras de seu amigo, então você pega itens basicos e vai ate a casa dele.");
+    alert("Ao chegar na casa dele, você rapidamente o chama e o convida para ir com você. Ele prontamente responde que aceita e logo em seguida vão embora de seu pais natal.");
+    alert("Agora, os dois tem que decidir entre ir para a etiopia ou para o sultão, dois paises vizinhos de Eritreia.");
+    let irLocal = prompt ("Para onde ir? (etiopia) (sultao)");
+
+    if(irLocal == "etiopia"){
+        goEtiopia()
+    } else if (irLocal == "sultao"){
+        goSultão()
+    } else{
+        alert("resposta invalida");
+    }
+    
 }
 
 function goAlone() {
-    showText("Você decidiu não chamar seu amigo e ir sozinho. Então você prontamente pega suas coisas e vai embora de seu pais natal.");
-    showText("Agora, você precisa decidir entre ir para a etiopia ou para o sultão, dois paises vizinhos de Eritreia")
-    showChoices([
-        { text: "Ir à etiopia", action: goEtiopiaAlone },
-        { text: "Ir à sultão", action: goSultãoAlone }
-    ]);
+    alert("Você decidiu não chamar seu amigo e ir sozinho. Então você prontamente pega suas coisas e vai embora de seu pais natal.");
+    alert("Agora, você precisa decidir entre ir para a etiopia ou para o sultão, dois paises vizinhos de Eritreia");
+    let irLocal = prompt ("Para onde ir? (etiopia) (sultao)");
+
+    if(irLocal == "etiopia"){
+        goEtiopiaAlone()
+    } else if(irLocal == "sultao"){
+        goSultãoAlone()
+    } else{
+        alert("resposta invalida");
+    }
 }
 
 function goEtiopiaAlone () {
-    showText("")
+    alert("Você decidiu ir embora para a etiopia");
+    alert("você caminha durante dias no sol ardente, sobrevive comendo migalhas e fugindo de policias. Tudo isso sozinho");
+    alert("Após muito tempo e sofrimento, você consegue finalmente chegar na etiopia. Agora você decide se abrigar em uma loja de conveniencia");
+    alert("como você passou muito tempo sozinho você acabou perdendo 10x sanidade");
+    alert("sanidade é um fator muito importante, caso sua sanidade chegue a 0, algo ruim acontecera");
+    alert("Na loja de conveniencia, você pode utilizar o telefone para contatar um parente");
+    let UsarFone = prompt ("Você usa o telefone? (sim) (nao)");
+
+    if(UsarFone == "sim"){
+        alert("Você decide utilizar o telefone e ligar para sua tia, uma pessoa muito amada que sempre cuidou de você");
+        alert("Ao ligar, sua tia lhe fala que ela está em israel e que você pode ir para la. Ela fala que lá existe um abrigo onde você pode viver e ter a chance de conseguir uma vida melhor");
+        alert("Apos ouvir oque ela disse, você com os olhos cheios de esperança decide ir para israel, em busca de algo melhor");
+    } else if(UsarFone == "nao"){
+        alert("Você decidiu nao usar o telefone e ficar na etiopia, na loja de conveniencia")
+        alert("Na etiopia, você passa por muitas dificuldades, tendo que trabalhar na loja de conveniencia durante horas e horas apenas para nao morrer de fome")
+        alert("Apos exaustivos dias, você descobre do dono da loja que existe um local em israel que você pode ficar e possivelmente ter uma chance de viver uma vida melhor")
+        alert("Apos descobrir isso, você com os olhos cheios de esperança decide ir para israel, em busca de algo melhor")
+    }
 }
 
 function goSultãoAlone () {
-    showText("")
+    
 }
 
 function goEtiopia() {
-    showText("Você foi para a esquerda e encontrou um tesouro!");
+    
 }
 
 function goSultão() {
-    showText("Você foi para a direita e encontrou um monstro!");
+    
 }
+
+startGame()
